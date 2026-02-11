89c31462d85f5548f8c20446e9bcb53a5d8d397d
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-11 16:42:19 +0000

    Finished logic for generating domain names.

--- src/app.js
+++ src/app.js
@@ -7,15 +7,23 @@ const domainNamePart1Options = ["my", "fast", "crazy"];
 const domainNamePart2Options = ["travel", "food", "ticket"];
 const domainNamePart3Options = ["deals", "bus", "trio"];
 
-function displayDomainName(domainNamePart1, domainNamePart2, domainNamePart3) {
-  
+function displayNewDomainName(
+  domainNamePart1,
+  domainNamePart2,
+  domainNamePart3,
+) {
+  const domainNameList = dosument.querySelector("#domainNameList");
+  const newListItem = document.createElement("li");
+  newListItem.textContent =
+    domainNamePart1[i] + domainNamePart2[j] + domainNamePart3[k];
+  domainNameList.appendChild(newListItem);
 }
 
 window.onload = function () {
   for (i = 0; i < 5; i++) {
     for (j = 0; j < 5; j++) {
       for (k = 0; k < 5; k++) {
-
+        displayDomainNewName(i, j, k);
       }
     }
   }
