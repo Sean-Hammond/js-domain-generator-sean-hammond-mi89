99a8ac9c16c17ec6ddda646d23c7810daff75167
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-11 16:48:29 +0000

    Moved functions inside onload function and made for loops dynamic baed on array lenths

--- src/app.js
+++ src/app.js
@@ -3,26 +3,26 @@ import "./style.css";
 
 import "./assets/img/dotJS-logo.jpg";
 
-const domainNamePart1Options = ["my", "fast", "crazy"];
-const domainNamePart2Options = ["travel", "food", "ticket"];
-const domainNamePart3Options = ["deals", "bus", "trio"];
-
-function displayNewDomainName(
-  domainNamePart1,
-  domainNamePart2,
-  domainNamePart3,
-) {
-  const domainNameList = dosument.querySelector("#domainNameList");
-  const newListItem = document.createElement("li");
-  newListItem.textContent =
-    domainNamePart1[i] + domainNamePart2[j] + domainNamePart3[k];
-  domainNameList.appendChild(newListItem);
-}
-
 window.onload = function () {
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 5; j++) {
-      for (k = 0; k < 5; k++) {
+  const domainNamePart1Options = ["my", "fast", "crazy"];
+  const domainNamePart2Options = ["travel", "food", "ticket"];
+  const domainNamePart3Options = ["deals", "bus", "trio"];
+
+  function displayNewDomainName(
+    domainNamePart1,
+    domainNamePart2,
+    domainNamePart3,
+  ) {
+    const domainNameList = dosument.querySelector("#domainNameList");
+    const newListItem = document.createElement("li");
+    newListItem.textContent =
+      domainNamePart1[i] + domainNamePart2[j] + domainNamePart3[k];
+    domainNameList.appendChild(newListItem);
+  }
+
+  for (i = 0; i < domainNamePart1Options.length; i++) {
+    for (j = 0; j < domainNamePart2Options.length; j++) {
+      for (k = 0; k < domainNamePart3Options.length; k++) {
         displayNewDomainName(i, j, k);
       }
     }
