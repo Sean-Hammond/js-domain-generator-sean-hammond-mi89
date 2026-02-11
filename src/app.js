b3ee011be959119fbb2ddbc587ad921e941991c9
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-11 18:54:42 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import "./style.css";
 import "./assets/img/dotJS-logo.jpg";
 
 window.onload = function () {
-  const domainPt1 = ["magic", "fast", "crazy"];
-  const domainPt2 = ["super", "food", "school"];
-  const domainPt3 = ["man", "b.us", "mar.io"];
+  const domainPt1 = ["Magic", "Fast", "Crazy"];
+  const domainPt2 = ["Super", "Food", "School"];
+  const domainPt3 = ["Man", "b.us", "Mar.io"];
   const domainExtensions = ["com", "net", "org"];
 
   let thereAreDuplicatesToSkip = false;
@@ -14,7 +14,7 @@ window.onload = function () {
   function createElement(i, j, k, x) {
     const domainList = document.querySelector("#domainList");
     const liElement = document.createElement("li");
-    if ((domainPt3[k] === "b.us") || (domainPt3[k] === "mar.io")) {
+    if (domainPt3[k] === "b.us" || domainPt3[k] === "Mar.io") {
       liElement.textContent = domainPt1[i] + domainPt2[j] + domainPt3[k];
       thereAreDuplicatesToSkip = true;
     } else {
@@ -30,33 +30,11 @@ window.onload = function () {
         for (let x = 0; x < domainPt3.length; x++) {
           createElement(i, j, k, x);
           if (thereAreDuplicatesToSkip) {
-            x = 0;
-            k++;
+            x = domainPt3.length;
             thereAreDuplicatesToSkip = false;
           }
         }
       }
     }
   }
-
-  // const domainNamePart1Options = ["my", "fast", "crazy"];
-  // const domainNamePart2Options = ["travel", "food", "ticket"];
-  // const domainNamePart3Options = ["deals", "bus", "trio"];
-  // function displayNewDomainName(i, j, k) {
-  //   const domainNameList = document.querySelector("#domainNameList");
-  //   const newListItem = document.createElement("li");
-  //   newListItem.textContent =
-  //     domainNamePart1Options[i] +
-  //     domainNamePart2Options[j] +
-  //     domainNamePart3Options[k];
-  //   domainNameList.appendChild(newListItem);
-  //   console.log(newListItem)
-  // }
-  // for (i = 0; i < domainNamePart1Options.length; i++) {
-  //   for (j = 0; j < domainNamePart2Options.length; j++) {
-  //     for (k = 0; k < domainNamePart3Options.length; k++) {
-  //       displayNewDomainName(i, j, k);
-  //     }
-  //   }
-  // }
 };
